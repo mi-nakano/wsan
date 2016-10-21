be9430f2b4cdc8fc5bfbb6a4f102ab98ef5b74a4
MizukiNakano <mizuki590@gmail.com>
2016-10-21 10:35:43 +0900

    テストを修正

--- test/sensor_test.exs
+++ test/sensor_test.exs
@@ -6,7 +6,9 @@ defmodule SensorTest do
   test "sensor test" do
     pid = Sensor.start(self, 0)
     receive do
-      {:msg, sensorPid, event} -> assert(1<= event.value and event.value <= 5)
+      {:msg, sensor_pid, event} ->
+        assert(1<= event.value and event.value <= 5)
+        assert sensor_pid == pid
     end
   end
 end
