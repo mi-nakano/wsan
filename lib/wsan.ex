2788c4a7f10c9109ecdac8236513ebc94d3c5555
MizukiNakano <mizuki590@gmail.com>
2016-10-31 13:05:07 +0900

    fix bug

--- lib/wsan.ex
+++ lib/wsan.ex
@@ -2,9 +2,9 @@ defmodule Wsan do
   import Actor
 
   def start() do
-    actors = for n <- 1..3 do
+    actors = for n <- 1..2 do
       actor = Wsan.Router.route(1, SimpleActor, :spawn, [])
-      for n <- 1..3, do: Wsan.Router.route(2, Sensor, :start, [actor, n])
+      for m <- 1..2, do: Wsan.Router.route(2, Sensor, :start, [actor, m])
       actor
     end
 
