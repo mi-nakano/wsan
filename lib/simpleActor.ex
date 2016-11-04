0b48cf5b46760e3ba0d7983d0b712010e4e1e1c9
MizukiNakano <mizuki590@gmail.com>
2016-11-04 11:08:31 +0900

    logging

--- lib/simpleActor.ex
+++ lib/simpleActor.ex
@@ -1,15 +1,16 @@
 defmodule SimpleActor do
   use ContextEX
   use Actor
+  require Logger
 
 
   deflf routine(msg), %{:categoryA => :layer1} do
-    IO.puts "msg came @layer1!!!!!!!!!"
-    IO.inspect msg
+    Logger.info "msg came @layer1!!!!!!!!!"
+    Logger.info inspect(msg)
   end
   deflf routine(msg) do
-    IO.puts "msg came @default"
-    IO.inspect msg
+    Logger.info "msg came @default"
+    Logger.info inspect(msg)
 
     if (msg.value == 5), do: activate_layer(%{:categoryA => :layer1})
   end
