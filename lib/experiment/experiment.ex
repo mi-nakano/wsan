8c79984b7031f024e9cb84ebf568c54c2ab68152
MizukiNakano <mizuki590@gmail.com>
2016-11-11 17:51:29 +0900

    pingpong

--- lib/experiment/experiment.ex
+++ lib/experiment/experiment.ex
@@ -1,21 +1,42 @@
 defmodule Experiment do
   require Wsan
+  require Wsan.Router
+  use ContextEX
 
   @num 100
 
-  def start() do
+  def start_pingpong() do
+    # prepare
+    n1 = Wsan.Router.route(1, Experiment, :pingpong, [self])
+    n2 = Wsan.Router.route(2, Experiment, :pingpong, [self])
+    IO.write "n1="
+    IO.inspect n1
+    IO.write "n2="
+    IO.inspect n2
+
+    # measure
     prev = System.os_time()
 
     # do something
-    for n <- 1..@num do
-      IO.write "."
+    send n1, {n2, @num}
+    receive do
+      :ok -> :ok
     end
-    Wsan.start
 
+    # measure
     diff = System.os_time() - prev
-
-    IO.puts ""
     IO.write "Time: "
     IO.inspect diff
+  end
+
+  # n=0 になるまで減算、0になったらparentに送信
+  def pingpong(parent) do
+    receive do
+      {_, 0} ->
+        send parent, :ok
+      {pid, n} ->
+        send pid, {self, n-1}
+        pingpong(parent)
+    end
   end
 end
