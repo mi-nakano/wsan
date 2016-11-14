581deb6ea9c44c4c5e09a10805086600df6f5f6a
MizukiNakano <mizuki590@gmail.com>
2016-11-14 09:23:01 +0900

    refactoring

--- lib/experiment/experiment.ex
+++ lib/experiment/experiment.ex
@@ -4,28 +4,31 @@ defmodule Experiment do
 
   @num 100
 
+  # funcを実行し、それにかかった時間を出力
+  def measure(func, args \\ []) do
+    prev = System.os_time()
+
+    # do something
+    apply(func, args)
+
+    diff = System.os_time() - prev
+    IO.write "Time: "
+    IO.inspect diff
+    diff
+  end
+
   def start_pingpong() do
     # prepare
     n1 = Wsan.Router.route(1, Experiment, :pingpong, [self])
     n2 = Wsan.Router.route(2, Experiment, :pingpong, [self])
-    IO.write "n1="
-    IO.inspect n1
-    IO.write "n2="
-    IO.inspect n2
-
-    # measure
-    prev = System.os_time()
 
     # do something
-    send n1, {n2, @num}
-    receive do
-      :ok -> :ok
-    end
-
-    # measure
-    diff = System.os_time() - prev
-    IO.write "Time: "
-    IO.inspect diff
+    measure(fn ->
+      send n1, {n2, @num}
+      receive do
+        :ok -> :ok
+      end
+    end)
   end
 
   # n=0 になるまで減算、0になったらparentに送信
