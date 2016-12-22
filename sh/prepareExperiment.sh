cc408f5f088b25774d0c54a4b5195af971f73de1
MizukiNakano <mizuki590@gmail.com>
2016-12-22 16:39:40 +0900

    rm sh

--- sh/prepareExperiment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd `pwd` > /dev/null
-cd `dirname $0`
-
-sh ./removeLog.sh
-
-popd > /dev/null
--- sh/removeLog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pushd `pwd` > /dev/null
-cd `dirname $0`
-cd ../
-
-echo "remove *.log"
-find . -name "*.log" -type f | xargs -J% rm %
-
-popd > /dev/null
