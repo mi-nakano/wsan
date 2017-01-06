1cde6176fb4b36f680b108a29a4f19c749a18426
MizukiNakano <mizuki590@gmail.com>
2017-01-06 13:35:12 +0900

    rename

--- lib/experiment/experiment.ex
+++ lib/experiment/experiment.ex
@@ -9,7 +9,7 @@ defmodule Experiment do
   @num_pairs 100
   @num_process 100
 
-  defp do_experimet(num, output_file, measure_func, args \\ []) do
+  defp do_experiment(num, output_file, measure_func, args) do
     unless (File.exists? @log_dir), do: File.mkdir @log_dir
     file_path = @log_dir <> output_file
     File.write(file_path, "")
@@ -27,10 +27,10 @@ defmodule Experiment do
     IO.puts "num_repeat = #{@num_repeat}"
     IO.puts "num_token = #{@num_token}"
     IO.puts "----- pingpong -----"
-    do_experimet(@num_repeat, "pingpong-#{@num_token}token.log", &Experiment.Pingpong.measure_pingpong/1, [@num_token])
+    do_experiment(@num_repeat, "pingpong-#{@num_token}token.log", &Experiment.Pingpong.measure_pingpong/1, [@num_token])
 
     IO.puts "----- pingpong (layered) -----"
-    do_experimet(@num_repeat, "pingpong-#{@num_token}token_layered.log", &Experiment.Pingpong.measure_pingpong_lf/1, [@num_token])
+    do_experiment(@num_repeat, "pingpong-#{@num_token}token_layered.log", &Experiment.Pingpong.measure_pingpong_lf/1, [@num_token])
   end
 
   def experiment2(num_pairs \\ @num_pairs) do
@@ -39,10 +39,10 @@ defmodule Experiment do
     IO.puts "num_token = #{@num_token}"
     IO.puts "num_pairs = #{num_pairs}"
     IO.puts "----- pingpong #{num_pairs} pair-----"
-    do_experimet(@num_repeat, "pingpong-#{@num_token}token-#{num_pairs}pair.log", &Experiment.Pingpong.measure_pingpong_multiple/2, [@num_token, num_pairs])
+    do_experiment(@num_repeat, "pingpong-#{@num_token}token-#{num_pairs}pair.log", &Experiment.Pingpong.measure_pingpong_multiple/2, [@num_token, num_pairs])
 
     IO.puts "----- pingpong #{num_pairs} pair (layered) -----"
-    do_experimet(@num_repeat, "pingpong-#{@num_token}token-#{num_pairs}pair_layered.log", &Experiment.Pingpong.measure_pingpong_multiple_lf/2, [@num_token, num_pairs])
+    do_experiment(@num_repeat, "pingpong-#{@num_token}token-#{num_pairs}pair_layered.log", &Experiment.Pingpong.measure_pingpong_multiple_lf/2, [@num_token, num_pairs])
   end
 
   def experiment_activation(num_process \\ @num_process) do
@@ -50,8 +50,8 @@ defmodule Experiment do
     IO.puts "num_repeat = #{@num_repeat}"
     IO.puts "num_process = #{num_process}"
     IO.puts "----- comparison experiment -----"
-    do_experimet(@num_repeat, "activation-#{num_process}process_comparison.log", &Experiment.Activation.measure_activation_comparison/1, [num_process])
+    do_experiment(@num_repeat, "activation-#{num_process}process_comparison.log", &Experiment.Activation.measure_activation_comparison/1, [num_process])
     IO.puts "----- group activation -----"
-    do_experimet(@num_repeat, "activation-#{num_process}.log", &Experiment.Activation.measure_activation/1, [num_process])
+    do_experiment(@num_repeat, "activation-#{num_process}.log", &Experiment.Activation.measure_activation/1, [num_process])
   end
 end
