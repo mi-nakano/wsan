1eabaecc53b53bf57bf72c82a31757f9dd2411f3
MizukiNakano <mizuki590@gmail.com>
2017-01-09 19:04:13 +0900

    median

--- lib/experiment/analyzer.ex
+++ lib/experiment/analyzer.ex
@@ -1,4 +1,5 @@
 defmodule Experiment.Analyzer do
+  require Integer
 
   # funcを実行し、それにかかった時間を出力
   def measure(func, args \\ []) do
@@ -21,6 +22,7 @@ defmodule Experiment.Analyzer do
 
     count = Enum.count(list)
     sum = list |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+    median = calc_median(list)
 
     IO.write "COUNT: "
     IO.inspect count
@@ -28,11 +30,25 @@ defmodule Experiment.Analyzer do
     IO.inspect sum
     IO.write "AVG(milliSec): "
     IO.inspect(sum / count)
+    IO.write "Med(milliSec): "
+    IO.inspect median
     :ok
   end
 
   # 時間の単位をmsに変換
   def convert(time) do
     System.convert_time_unit(time, :native, :milliseconds)
+  end
+
+  def calc_median(list) do
+    count = Enum.count(list)
+    sorted = Enum.sort(list)
+    index = div(count, 2) - 1
+    if Integer.is_even(count) do
+      sum = Enum.at(sorted, index) + Enum.at(sorted, index+1)
+      sum / 2
+    else
+      Enum.at(sorted, index+1)
+    end
   end
 end
